18464c2e7821ea6d2199b9c6015d6b49c66b26d9
Peter Dolenc <pe.dolenc@gmail.com>
2018-04-11 19:16:12 +0200

    Dominant color extraction and background color fill

--- src/image_helper.py
+++ src/image_helper.py
@@ -8,46 +8,25 @@ from entities.image_meta import ImageMeta
 
 class ImageHelper:
 
+    # resizes the image to target dimensions
     def resize(image: pygame.Surface,  dimensions) -> pygame.Surface:
         dimensions = (int(dimensions[0]), int(dimensions[1]))
         return pygame.transform.smoothscale(image, dimensions)
 
-    def get_dominant_colors(image_meta: ImageMeta, image:pygame.Surface, color_thief=True) -> List[Tuple[int]]:
-        if color_thief:
-            ct = ColorThief(image_meta.full_path)
-            colors = ct.get_palette(10, 10)
-            hsvs = [ (*colorsys.rgb_to_hsv(*colors[i]), i) for i in range(len(colors)) ]
-            hsvs.sort(key=lambda c: ImageHelper.grade_color(*c), reverse=True)
-            rgbs = [ colorsys.hsv_to_rgb(c[0], c[1], c[2]) for c in hsvs]
-            return rgbs
-        else:
-            pixarray = pygame.PixelArray(image)
-            colors = []
-            color_steps = 6
-            color_gap = int(256.0 / float(color_steps) + 0.5)
+    # gets the dominant colors
+    # uses color thief to extract them
+    # then sorts them by color intensity and occurrence
+    def get_dominant_colors(image_meta: ImageMeta, image:pygame.Surface, colour_count=10, sampling=10) -> List[Tuple[int]]:
+        ct = ColorThief(image_meta.full_path)
+        colors = ct.get_palette(colour_count, sampling)
+        hsvs = [ (*colorsys.rgb_to_hsv(*colors[i]), i) for i in range(len(colors)) ]
+        hsvs.sort(key=lambda c: ImageHelper.grade_color(*c), reverse=True)
+        rgbs = [ colorsys.hsv_to_rgb(c[0], c[1], c[2]) for c in hsvs]
+        return rgbs
 
-            for i in range(pixarray.shape[0]):
-                if (i % 3 == 0):
-                    for j in range(pixarray.shape[1]):
-                        if (j % 3 == 0):
-                            # rgbc = pygame.Color(pixarray[i,j]*256 + 255)
-                            pixel = image.get_at((i, j))
-                            r = pixel[0]
-                            g = pixel[1]
-                            b = pixel[2]
-                            colorint = r / color_gap * color_steps * color_steps + g / color_gap * color_steps + b / color_gap
-                            colors.append(colorint)
-
-            fcolor = ImageHelper.most_common(colors)
-
-            dominant_color = pygame.Color(((fcolor / color_steps / color_steps) % color_steps) * color_gap + color_gap / 2,
-                                          ((fcolor / color_steps) % color_steps) * color_gap + color_gap / 2, ((fcolor) % color_steps) * color_gap + color_gap / 2, 255)
-
-            return [ dominant_color ]
-
-    def most_common(lst: List):
-        return max(set(lst), key=lst.count)
-
+    # grades a HSV color with occurrence i
+    # if the color is very dark or very washed out then score is zero
+    # otherwise its a combination of intensity and saturation and occurrence
     def grade_color(h, s ,v, i):
         v = float(v) / 256
         if s < 0.1 or v < 0.1 or s + v < 0.3:
