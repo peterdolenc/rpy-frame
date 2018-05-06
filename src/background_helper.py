ab31534828bf7426b45a51d00865307bf53f00a3
Peter Dolenc <pe.dolenc@gmail.com>
2018-05-06 11:24:30 +0200

    Pattern generator extracted and refactored

--- src/background_helper.py
+++ src/background_helper.py
@@ -1,215 +1,20 @@
 from __future__ import division
 import pygame
 from typing import List, Tuple
-import matplotlib, math, scipy, random, os
-import matplotlib.backends.backend_agg as agg
-import matplotlib.pyplot as plt
-from matplotlib import patches
 
-'''
-Functions to define all polygons used in each pattern.
-If something is breaking it is most likely addShape().
-You must pass a very specific list to this function as described below.
-'''
-
-ar = scipy.array
-
-
-def Rotate2D(pts, cnt, ang=scipy.pi / 4):
-    '''pts = {} Rotates points(nx2) about center cnt(2) by angle ang(1) in radian'''
-    return scipy.dot(pts - cnt, ar([[scipy.cos(ang), scipy.sin(ang)],
-                                    [-scipy.sin(ang), scipy.cos(ang)]])) + cnt
-
-
-def solveForLeg(h, leg1):
-    '''pythagorean theorum to solve for leg (not hypotenuse)'''
-    return (math.sqrt(h * h - leg1 * leg1))
-
-
-def addShape(points, degrees=0, alphaParam=1, ec='none', l=0, jn='round'):
-    '''Finalize rotation and add shape to plot.'''
-    # "points" should consist of the list returned from any of the
-    # geometry functions below (side3, side4, etc.)
-    origin = points[-2]
-    color = points[-1]
-    newPoints = list(points)
-    del newPoints[-1]
-    del newPoints[-1]
-
-    pts = ar(newPoints)
-    radians = degrees * scipy.pi / 180
-    ots = Rotate2D(pts, ar([origin]), radians)
-    sub1.add_patch(patches.Polygon(ots, fc=color, ec=ec,
-                                   alpha=alphaParam, joinstyle=jn, lw=l, rasterized=True))
-
-
-def side3(w, oX, oY, c, e=0):
-    '''Makes a polygon with 3 sides of length w, centered around the origin'''
-    base = solveForLeg(w, w / float(2))
-    p1 = [oX + w / float(2), oY - ((1 / float(3)) * base)]
-    p2 = [oX, oY + (2 / float(3)) * base]
-    p3 = [oX - w / float(2), oY - ((1 / float(3)) * base)]
-    return ([p1, p2, p3, [oX, oY], c])
-
-
-def side4(w, oX, oY, c, e=0):
-    '''Makes a polygon with 4 sides of length w, centered around the origin.'''
-    p1 = [oX - w / float(2), oY - w / float(2)]
-    p2 = [oX - (w - e) / float(2), oY + (w - e) / float(2)]
-    p3 = [oX + w / float(2), oY + w / float(2)]
-    p4 = [oX + (w - e) / float(2), oY - (w - e) / float(2)]
-    return ([p1, p2, p3, p4, [oX, oY], c])
-
-
-def side6(w, oX, oY, c, e=0):
-    '''Makes a polygon with 6 sides of length w, centered around the origin.'''
-    d = solveForLeg(w, w / float(2))
-    de = solveForLeg(w - e, (w - e) / float(2))
-    p1 = [oX, oY + w]
-    p2 = [oX + de, oY + (w - e) / float(2)]
-    p3 = [oX + d, oY - w / float(2)]
-    p4 = [oX, oY - (w - e)]
-    p5 = [oX - d, oY - w / float(2)]
-    p6 = [oX - de, oY + (w - e) / float(2)]
-    return ([p1, p2, p3, p4, p5, p6, [oX, oY], c])
-
-
-def side8(w, oX, oY, c, e=0):
-    '''Makes a polygon with 8 sides of length w, centered around the origin.'''
-    pts = side4(math.sqrt(2) * w, oX, oY, c)
-    pts2 = side4(math.sqrt(2) * w - e, oX, oY, c)
-    del pts2[-1]
-    del pts2[-1]
-    ots = Rotate2D(pts2, ar([oX, oY]), 45 * scipy.pi / 180).tolist()
-    return ([pts[0], ots[0], pts[3], ots[3], pts[2],
-             ots[2], pts[1], ots[1], [oX, oY], c])
-
-
-def side12(w, oX, oY, c, e=0):
-    '''Makes a polygon with 12 sides, centered around the origin.'''
-    # w is not the side length for this function
-    pts = side6(w, oX, oY, c)
-    pts2 = side6(w - e, oX, oY, c)
-    del pts2[-1]
-    del pts2[-1]
-    ots = Rotate2D(pts2, ar([oX, oY]), 30 * scipy.pi / 180).tolist()
-    return ([pts[0], ots[0], pts[5], ots[5], pts[4], ots[4],
-             pts[3], ots[3], pts[2], ots[2], pts[1], ots[1], [oX, oY], c])
-
-
-# diamond functions are for the box pattern only
-def diamondA(w, oX, oY, c='#000000', e=0):
-    d = math.sqrt(w * w - ((w / float(2)) * (w / float(2))))
-    p1 = [oX, oY]
-    p2 = [oX, oY + w - e]
-    p3 = [oX - d, oY + w - e + (w / float(2))]
-    p4 = [oX - d, oY + (w / float(2))]
-    return ([p1, p2, p3, p4, [oX, oY], c])
-
-
-def diamondB(w, oX, oY, c='#000000', e=0):
-    d = math.sqrt(w * w - ((w / float(2)) * (w / float(2))))
-    p1 = [oX, oY]
-    p2 = [oX, oY + w - e]
-    p3 = [oX + d, oY + w - e + (w / float(2))]
-    p4 = [oX + d, oY + (w / float(2))]
-    return ([p1, p2, p3, p4, [oX, oY], c])
-
-
-def diamondC(w, oX, oY, c='#000000', e=0):
-    d = math.sqrt(w * w - ((w / float(2)) * (w / float(2))))
-    p1 = [oX, oY + e]
-    p2 = [oX, oY + w]
-    p3 = [oX - d, oY + w + (w / float(2))]
-    p4 = [oX - d, oY + (w / float(2)) + e]
-    return ([p1, p2, p3, p4, [oX, oY], c])
-
-
-def diamondD(w, oX, oY, c='#000000', e=0):
-    d = math.sqrt(w * w - ((w / float(2)) * (w / float(2))))
-    p1 = [oX, oY + e]
-    p2 = [oX, oY + w]
-    p3 = [oX + d, oY + w + (w / float(2))]
-    p4 = [oX + d, oY + (w / float(2)) + e]
-    return ([p1, p2, p3, p4, [oX, oY], c])
-
-
-def circleSize(c1, c2, c3, c4, c5, width, height):
-    '''Pattern 3: Circle Size'''
-    global fig
-    global sub1 # allow other functions to add shapes to the plot
-
-    fig = plt.figure(figsize=(10,10), dpi=int(width/10+1))
-    plt.subplots_adjust(hspace=0, wspace=0)
-
-    ff = [[c5, c2, c4, c2, c5, c1, c5],[c2, c4, c1, c4, c2, c5, c2],
-          [c4, c1, c1, c1, c4, c2, c4],[c2, c4, c1, c4, c2, c5, c2],
-          [c5, c2, c4, c2, c5, c1, c5],[c1, c5, c2, c5, c1, c1, c1],
-          [c5, c2, c4, c2, c5, c1, c5]]
-
-    f2 = [[c4, c1, c5, c1, c4, c2, c4],[c1, c5, c2, c5, c1, c4, c1],
-          [c5, c2, c2, c2, c5, c1, c5],[c1, c5, c2, c5, c1, c4, c1],
-          [c4, c1, c5, c1, c4, c2, c4],[c2, c4, c1, c4, c2, c2, c2],
-          [c4, c1, c5, c1, c4, c2, c4]]
-
-    aa = [[c1, c5, c2, c2, c5, c1, c1],[c5, c2, c4, c4, c2, c5, c5],
-          [c2, c4, c1, c1, c4, c2, c2],[c2, c4, c1, c1, c4, c2, c2],
-          [c5, c2, c4, c4, c2, c5, c5],[c1, c5, c2, c2, c5, c1, c1],
-          [c1, c5, c2, c2, c5, c1, c1]]
-
-    rr = [10, 10, 10, 10, 15, 20, 25, 30, 30, 30, 25, 20, 15]
-
-    rrx = random.randint(0, len(rr)-1)
-
-    sub1 = fig.add_axes((0,0,1,1))
-    sub1.xaxis.set_visible(False)
-    sub1.yaxis.set_visible(False)
-    sub1.set_xlim([0, 480])
-    sub1.set_ylim([0, 480])
-    sub1.axis('off')
-    alpha = 0.5
-    for x in range(0, 8):
-        for y in range (0, 8):
-            xofs = x * 70
-            yofs = y * 70
-            sub1.add_patch(patches.Rectangle((xofs, yofs), 70, 70, fc=(0.5, 0.5, 0.5), alpha=1, ec='none'))
-            sub1.add_patch(patches.Rectangle((xofs, yofs), 70, 70, fc=c3, alpha=alpha, ec='none'))
-            for i in range(0, len(ff)):
-                count = 1
-                for a in range(0, len(ff[0])):
-                    sub1.add_patch(patches.Circle((5*count + xofs, 5+2*5*i + yofs), 5, fc=ff[i][a], alpha=alpha, ec='none'))
-                    sub1.add_patch(patches.Circle((5*count + xofs, 5+2*5*i + yofs), 3.5, fc=f2[i][a], alpha=random.randint(75, 100)/float(100) * alpha, ec='none'))
-                    sub1.add_patch(patches.Circle((5*count-5 + xofs, 2*5*i + yofs),1.5, fc=aa[i][a], alpha=0.75 * alpha, ec='none'))
-                    sub1.add_patch(patches.Circle((5*count + xofs, 5+2*5*i + yofs), random.randint(10, rr[rrx])/float(10), fc=c3, alpha=random.randint(50, 85)/float(100) * alpha, ec='none'))
-                    sub1.add_patch(patches.Circle((5*count + xofs, 5+2*5*i + yofs), 1, fc=c3, alpha=alpha, ec='none'))
-                    count = count + 2
-
-    canvas = agg.FigureCanvasAgg(fig)
-    canvas.draw()
-    renderer = canvas.get_renderer()
-    raw_data = renderer.tostring_rgb()
-
-    size = canvas.get_width_height()
-
-    # Create pygame surface from string
-    surf = pygame.image.fromstring(raw_data, size, "RGB")
-    plt.clf()
-
-
-    plt.close('all')
-
-    return surf
-
-
-def to_hex(rgb):
-    rgb = tuple([ int(rgb[i]) for i in range (0, len(rgb)) ])
-    return '#%02x%02x%02x' % rgb
+from pattern_generator import PatternGenerator
 
 
 class BackgroundHelper:
 
     def __init__(self, display_mode: List[int]):
         self.display_mode = display_mode
+        self.pattern_generator = PatternGenerator(display_mode)
+
+    @staticmethod
+    def to_hex(rgb):
+        rgb = tuple([int(rgb[i]) for i in range(0, len(rgb))])
+        return '#%02x%02x%02x' % rgb
 
     def get_dominant_color_fill(self, colors: List[Tuple[int]]) -> pygame.Surface:
         surface = pygame.Surface(self.display_mode)
@@ -217,7 +22,8 @@ class BackgroundHelper:
         return surface
 
     def get_dominant_pattern(self, colors: List[Tuple[int]]) -> pygame.Surface:
-        surface = circleSize(to_hex(colors[0]), to_hex(colors[1]), to_hex(colors[2]), to_hex(colors[3]), to_hex(colors[4]), self.display_mode[0], self.display_mode[1])
+        hex_colors = (self.to_hex(c) for c in colors[:5])
+        surface = self.pattern_generator.playful_circles(*hex_colors)
 
         return surface
 
---
+++ src/pattern_generator.py
@@ -0,0 +1,151 @@
+import pygame
+from typing import List
+import scipy
+import random
+import matplotlib.backends.backend_agg as agg
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+'''
+ Idea and base for the code taken from: https://github.com/eleanorlutz/AnimatedPythonPatterns/blob/master/PatternMaker.ipynb
+ Might be replaced in the future by pure surfaces solution instead of plotting
+'''
+ar = scipy.array
+
+
+class PatternGenerator:
+
+    def __init__(self, display_mode: List[int]):
+        self.display_mode = display_mode
+
+    # creates tne subplot on the figure that has no spacing around
+    @staticmethod
+    def create_subplot(fig, range_x, range_y, background_color, alpha):
+        subplot = fig.add_axes((0, 0, 1, 1))
+        subplot.xaxis.set_visible(False)
+        subplot.yaxis.set_visible(False)
+        subplot.set_xlim([0, range_x])
+        subplot.set_ylim([0, range_y])
+        subplot.axis('off')
+        neutral_gray = (0.5, 0.5, 0.5)
+        subplot.add_patch(patches.Rectangle((0, 0), range_x, range_y, fc=neutral_gray, alpha=1, ec='none'))
+        subplot.add_patch(patches.Rectangle((0, 0), range_x, range_y, fc=background_color, alpha=alpha, ec='none'))
+        return subplot
+
+    # converts the plot to a pygame surface
+    @staticmethod
+    def surface_from_plot(fig):
+        canvas = agg.FigureCanvasAgg(fig)
+        canvas.draw()
+        renderer = canvas.get_renderer()
+        raw_data = renderer.tostring_rgb()
+        size = canvas.get_width_height()
+        surf = pygame.image.fromstring(raw_data, size, "RGB")
+        return surf
+
+    # returns a random float in the range
+    @staticmethod
+    def randf(min, max):
+        return random.random() * (max-min) + min
+
+    # Displays different sized circles
+    def playful_circles(self, C, B, A, D, E, animation=None):
+
+        vertical_reps = 8
+        horizontal_reps = 8
+        rep_size = 60
+        alpha = 0.5
+        circle_radius = 5
+        alpha_rand1 = self.randf(0.75, 1.0)
+        alpha_rand2 = self.randf(0.5, 0.85)
+        N = 0x0000001
+
+        fig = plt.figure(figsize=(10, 10), dpi=int(self.display_mode[0] / 10 + 1))
+        plt.subplots_adjust(hspace=0, wspace=0)
+
+        outer_ring_colors = [
+            [E, B, D, B, E, A, E],
+            [B, D, A, D, B, E, B],
+            [D, A, A, A, D, B, D],
+            [B, D, A, D, B, E, B],
+            [E, B, D, B, E, A, E],
+            [A, E, B, E, A, A, A],
+            [E, B, D, B, E, A, E]]
+
+        middle_ring_colors = [
+            [D, A, E, A, D, B, D],
+            [A, E, B, E, A, D, A],
+            [E, B, B, B, E, A, E],
+            [A, E, B, E, A, D, A],
+            [D, A, E, A, D, B, D],
+            [B, D, A, D, B, B, B],
+            [D, A, E, A, D, B, D]]
+
+        gap_dots_colors = [
+            [A, E, B, B, E, A, N],
+            [E, B, D, D, B, E, N],
+            [B, D, A, A, D, B, N],
+            [B, D, A, A, D, B, N],
+            [E, B, D, D, B, E, N],
+            [A, E, B, B, E, A, N],
+            [N, N, N, N, N, N, N]]
+
+        # inner dot radius - progression for animation inf enabled
+        inner_dot_radiuses = [0.2, 0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.6, 0.6, 0.6, 0.5, 0.4, 0.3]
+
+        # If animation is enabled create a continuous inner dot radius value from discrete points above
+        if animation is None:
+            inner_dot_max_radius = random.choice(inner_dot_radiuses)
+        else:
+            animation_stages = len(inner_dot_radiuses)  # 12
+            accurate_index = min(0.99999, animation) * (animation_stages-1)  # 7.81
+            bottom_index = int(accurate_index)  # 0.71 * 11 = 7.81 => 7
+            reminder = accurate_index - bottom_index  # 0.81
+            top_index = bottom_index + 1  # 8
+            inner_dot_max_radius = reminder * inner_dot_radiuses[top_index] + (1-reminder) * inner_dot_radiuses[bottom_index]
+
+        # prepare subplot
+        background_color = C
+        subplot = self.create_subplot(fig, horizontal_reps * rep_size, vertical_reps * rep_size, background_color, alpha)
+
+        for h_rep in range(0, horizontal_reps):
+            for v_rep in range(0, vertical_reps):
+                offset_x = h_rep * rep_size
+                offset_y = v_rep * rep_size
+
+                # i goes from 0 to number of color sets in ff (7) - rows
+                for i in range(0, len(outer_ring_colors)):
+
+                    # j goes from 0 to number of colors in every set (7) - columns
+                    for j in range(0, len(outer_ring_colors[0])):
+
+                        center_x = offset_x + circle_radius * (2*j + 1)
+                        center_y = offset_y + circle_radius * (2*i + 1)
+
+                        # outer circle
+                        subplot.add_patch(
+                            patches.Circle((center_x, center_y), circle_radius, fc=outer_ring_colors[i][j], alpha=alpha, ec='none'))
+
+                        # middle circle
+                        subplot.add_patch(
+                            patches.Circle((center_x, center_y), 0.7 * circle_radius, fc=middle_ring_colors[i][j], alpha=alpha_rand1*alpha, ec='none'))
+
+                        # small circle in the gap between
+                        if gap_dots_colors[i][j] != N:
+                            subplot.add_patch(
+                                patches.Circle((center_x - circle_radius, center_y - circle_radius), 0.3*circle_radius, fc=gap_dots_colors[i][j], alpha=0.75*alpha, ec='none'))
+
+                        # slightly less opaque random growing dot surround
+                        subplot.add_patch(
+                            patches.Circle((center_x, center_y), self.randf(0.2, inner_dot_max_radius) * circle_radius, fc=background_color, alpha=alpha_rand2 * alpha, ec='none'))
+
+                        # the dot
+                        subplot.add_patch(
+                            patches.Circle((center_x, center_y), 0.2*circle_radius, fc=background_color, alpha=alpha, ec='none'))
+
+        # create surface from plot and clean the plot
+        surf = self.surface_from_plot(fig)
+        plt.clf()
+        plt.close('all')
+
+        return surf
